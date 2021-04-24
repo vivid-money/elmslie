db050f3bf54369c941939b2db451f5715760b2c3
Daniil Klimchuk <dklimchuk@projectx019.com>
2021-04-24 16:22:00 +0300

    Use App Startup library for initialization

---
+++ elmslie-android/src/main/java/vivid/money/elmslie/android/logger/DefaultLoggerInitializer.kt
@@ -0,0 +1,16 @@
+package vivid.money.elmslie.android.logger
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import androidx.startup.Initializer
+import vivid.money.elmslie.core.config.ElmslieConfig
+
+object DefaultLoggerInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        val isDebug = 0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+        if (isDebug) ElmslieConfig.defaultDebugLogger() else ElmslieConfig.defaultReleaseLogger()
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf()
+}
