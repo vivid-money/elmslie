596312e484ca9287f1769094053acbbcfae08c99
Daniil <klimchuk.daniil@gmail.com>
2021-06-19 10:13:58 +0300

    Add module for compose compatibility

---
+++ elmslie-compose/src/main/java/vivid/money/elmslie/compose/ElmComponentFragment.kt
@@ -0,0 +1,28 @@
+package vivid.money.elmslie.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rxjava3.subscribeAsState
+import androidx.fragment.app.Fragment
+import vivid.money.elmslie.android.screen.ElmDelegate
+import vivid.money.elmslie.android.screen.ElmScreen
+import vivid.money.elmslie.android.storeholder.LifecycleAwareStoreHolder
+
+abstract class ElmComponentFragment<Event : Any, Effect : Any, State : Any> : Fragment(),
+    ElmDelegate<Event, Effect, State> {
+
+    @Suppress("LeakingThis", "UnusedPrivateMember")
+    private val elm = ElmScreen(this, lifecycle) { requireActivity() }
+
+    protected val store
+        get() = storeHolder.store
+
+    override val storeHolder = LifecycleAwareStoreHolder(lifecycle, ::createStore)
+
+    final override fun render(state: State) = Unit
+
+    @Composable
+    fun state() = store.states.subscribeAsState(initial = store.currentState)
+
+    @Composable
+    fun effect() = store.effects.map { EffectWithKey(it) }.subscribeAsState(initial = null)
+}
