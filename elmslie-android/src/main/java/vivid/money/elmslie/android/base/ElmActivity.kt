33010eb277fd25fe714068defd8c213799dc019e
Eugene Komarov <ekomarov@vivid.money>
2021-07-14 13:05:17 +0600

    Fix parent class StoreHodler initialisation inside ElmActivity and ElmFragment

--- elmslie-android/src/main/java/vivid/money/elmslie/android/base/ElmActivity.kt
+++ elmslie-android/src/main/java/vivid/money/elmslie/android/base/ElmActivity.kt
@@ -5,6 +5,7 @@ import vivid.money.elmslie.android.screen.ElmDelegate
 import vivid.money.elmslie.android.screen.ElmScreen
 import vivid.money.elmslie.android.storeholder.LifecycleAwareStoreHolder
 import vivid.money.elmslie.android.storeholder.StoreHolder
+import vivid.money.elmslie.android.util.fastLazy
 
 abstract class ElmActivity<Event : Any, Effect : Any, State : Any> :
     AppCompatActivity(), ElmDelegate<Event, Effect, State> {
@@ -15,5 +16,7 @@ abstract class ElmActivity<Event : Any, Effect : Any, State : Any> :
     protected val store
         get() = storeHolder.store
 
-    override val storeHolder: StoreHolder<Event, Effect, State> = LifecycleAwareStoreHolder(lifecycle, ::createStore)
+    override val storeHolder: StoreHolder<Event, Effect, State> by fastLazy {
+        LifecycleAwareStoreHolder(lifecycle, ::createStore)
+    }
 }
