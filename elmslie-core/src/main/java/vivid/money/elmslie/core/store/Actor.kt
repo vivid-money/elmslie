5d35d63d668a681b59b168ae8235102eae3152a9
Eugene Komarov <eugene.komarov@dif.tech>
2023-07-04 14:59:09 +0200

    remove logging from actor extension

--- elmslie-core/src/main/java/vivid/money/elmslie/core/store/Actor.kt
+++ elmslie-core/src/main/java/vivid/money/elmslie/core/store/Actor.kt
@@ -3,8 +3,6 @@ package vivid.money.elmslie.core.store
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.onEach
-import vivid.money.elmslie.core.config.ElmslieConfig
 
 abstract class Actor<Command : Any, Event : Any> {
 
@@ -16,17 +14,7 @@ abstract class Actor<Command : Any, Event : Any> {
 
     fun <T : Any> Flow<T>.mapEvents(
         eventMapper: (T) -> Event? = { null },
-        errorMapper: (error: Throwable) -> Event? = { null }
+        errorMapper: (error: Throwable) -> Event? = { null },
     ) = mapNotNull { eventMapper(it) }
-        .onEach { it.logSuccessEvent() }
-        .catch { it.logErrorEvent(errorMapper)?.let { event -> emit(event) } ?: throw it }
-
-    private fun Throwable.logErrorEvent(errorMapper: (Throwable) -> Event?): Event? {
-        return errorMapper(this).also {
-            ElmslieConfig.logger.nonfatal(error = this)
-            ElmslieConfig.logger.debug("Failed app state: $it")
-        }
-    }
-
-    private fun Event.logSuccessEvent() = ElmslieConfig.logger.debug("Completed app state: $this")
+        .catch { errorMapper(it) ?: throw it }
 }
