a962cde97306773bb69dc4f43dafbef4945cc7b0
Eugene Komarov <eugene.komarov@dif.tech>
2023-09-12 12:40:36 +0200

    add state param to store listener methods

--- elmslie-core/src/main/java/vivid/money/elmslie/core/store/StoreListener.kt
+++ elmslie-core/src/main/java/vivid/money/elmslie/core/store/StoreListener.kt
@@ -2,11 +2,11 @@ package vivid.money.elmslie.core.store
 
 interface StoreListener<Event : Any, State : Any, Effect : Any, Command : Any> {
 
-    fun onEvent(key: String, event: Event) {}
-    fun onEffect(key: String, effect: Effect) {}
-    fun onCommand(key: String, command: Command) {}
-    fun onStateChanged(key: String, newState: State, oldState: State, eventCause: Event) {}
+    fun onBeforeEvent(key: String, event: Event, currentState: State) {}
+    fun onAfterEvent(key: String, newState: State, oldState: State, eventCause: Event) {}
+    fun onEffect(key: String, effect: Effect, state: State) {}
+    fun onCommand(key: String, command: Command, state: State) {}
 
     fun onReducerError(key: String, throwable: Throwable, event: Event) {}
-    fun onCommandError(key: String, throwable: Throwable, command: Command) {}
+    fun onActorError(key: String, throwable: Throwable, command: Command) {}
 }
