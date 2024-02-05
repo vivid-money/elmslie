afddb2800b4fc001da9c31857029874a1898f0ab
Dmitriy Berdnikov <d.k.berdnikov@tinkoff.ru>
2024-02-05 15:45:49 +0300

    make switch extension for Flow inside Actors

--- samples/coroutines-loader/src/main/kotlin/money/vivid/elmslie/samples/coroutines/timer/elm/TimerActor.kt
+++ samples/coroutines-loader/src/main/kotlin/money/vivid/elmslie/samples/coroutines/timer/elm/TimerActor.kt
@@ -1,6 +1,7 @@
 package money.vivid.elmslie.samples.coroutines.timer.elm
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import money.vivid.elmslie.core.store.Actor
 import money.vivid.elmslie.core.switcher.Switcher
@@ -11,18 +12,20 @@ internal object TimerActor : Actor<Command, Event>() {
 
     override fun execute(command: Command) =
         when (command) {
-            is Command.Start ->
-                switcher
-                    .switch { secondsFlow() }
-                    .mapEvents(
-                        eventMapper = { Event.OnTimeTick },
-                        errorMapper = { Event.OnTimeError(it) },
-                    )
-            is Command.Stop -> switcher.cancel()
+            is Command.Start -> secondsFlow()
+                .switchOnEach(command)
+                .mapEvents(
+                    eventMapper = { Event.OnTimeTick },
+                    errorMapper = { Event.OnTimeError(it) },
+                )
+
+            is Command.Stop -> switchers.getOrPut(Command.Start::class) {
+                Switcher()
+            }.cancel()
         }
 
     @Suppress("MagicNumber")
-    private fun secondsFlow() = flow {
+    private fun secondsFlow(): Flow<Int> = flow {
         repeat(10) {
             delay(1000)
             emit(it)
