6d2c2fe194da7e42d0e1f676ac992ef20d36b40a
trdelnk <andrey.yakovlev@dif.tech>
2024-06-18 23:04:16 +0300

    add publish to maven workflow

--- .github/sh/validate_publishing_branch.sh
+++ .github/sh/validate_publishing_branch.sh
@@ -3,10 +3,10 @@
 if [[ ${GITHUB_REF##*/} == main ]]; then
   # Safe to publish from main branch
   exit 0
-elif [[ ${GITHUB_REF##*/} == publish* ]]; then
+elif [[ ${GITHUB_REF##*/} == release* ]]; then
   # Safe to publish from branches that have manually enabled publishing
   exit 0
 else
-  echo "::error ::Can only release from main branch or branches that start with 'publish'"
+  echo "::error ::Can only release from main branch or branches that start with 'release'"
   exit 1
 fi
